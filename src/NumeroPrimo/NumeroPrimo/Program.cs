f9bff77b93f8b9db6ada3d9798d7bf2ab7108b44
naiaragabriela <naigabi31@gmail.com>
2023-03-05 21:37:35 -0300

    descobrindo numeros primos com função

---
+++ src/NumeroPrimo/NumeroPrimo/Program.cs
@@ -0,0 +1,30 @@
+﻿double numero, i;
+bool ehPrimo = true;
+
+double NumeroDigitado()
+{
+    Console.WriteLine("Digite um número: ");
+    return double.Parse(Console.ReadLine());
+}
+
+numero = NumeroDigitado();
+
+void ResultadoPrimo()
+{
+    for (i = 2; i < numero; i++)
+    {
+        if (numero % i == 0)
+        {
+            ehPrimo = !ehPrimo;
+        }
+    }
+    if (ehPrimo && (numero != 0))
+    {
+        Console.WriteLine($"O número digitado {numero} é primo ");
+    }
+    else
+    {
+        Console.WriteLine($"O númeor digitado {numero} não é primo");
+    }
+}
+ResultadoPrimo();
